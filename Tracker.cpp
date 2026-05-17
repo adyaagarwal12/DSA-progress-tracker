b5522b5612539fc66d0774f91b370558e95beb8f
Adya Agarwal <adyaagarwal12@gmail.com>
2026-05-17 18:18:38 +0530

    Implemented add problem functionality

---
+++ Tracker.cpp
@@ -0,0 +1,23 @@
+#include "Tracker.h"
+
+void Tracker::addProblem()
+{
+    Problem p;
+
+    cout<<"Enter title: ";
+    cin.ignore();
+    getline(cin,p.title);
+
+    cout<<"Enter topic: ";
+    getline(cin,p.topic);
+
+    cout<<"Enter difficulty: ";
+    getline(cin,p.difficulty);
+
+    cout<<"Solved? (1/0): ";
+    cin>>p.solved;
+
+    problems.push_back(p);
+
+    cout<<"Problem Added Successfully\n";
+}
